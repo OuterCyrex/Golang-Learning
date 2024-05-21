48a10721e88db73cfbe699d8f79d0a57e46d8d25
OuterCyrex <167676294+OuterCyrex@users.noreply.github.com>
2024-05-21 19:51:57 +0800

    Test.go
    goroutine

--- test.go
+++ test.go
@@ -26,23 +26,17 @@ func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, err
 */
 
 func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
 	value, ok := g.m[key]
 	if !ok {
-		g.mu.Lock()
 		newVal, err := fn()
 		g.m[key] = &call{wg: sync.WaitGroup{}, val: newVal, err: err}
-		g.mu.Unlock()
-		g.m[key].wg.Add(1)
-		defer g.m[key].wg.Done()
 		return newVal, err
 	} else {
-		value.wg.Wait()
 		return value.val, value.err
 	}
 }
-
-//思路：在多个线程访问Do方法时，每当一个goroutine输入一个Key值，先判断该Key值是否在Group.map[string]内存在，若不存在，则创造一个新的映射
-//若已存在该映射，则等待所有该映射key值对应的fn全部运行完，再输出该Key对应fn输出的空接口和err值
 
 func TestFun() (interface{}, error) {
 	time.Sleep(1 * time.Second)
